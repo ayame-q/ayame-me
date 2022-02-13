0ee6547456aa8c2699fa72632405366ef5f36ded
Ayame Tatematsu <ayame.space@gmail.com>
2022-02-14 00:29:53 +0900

    ContentEditableを削除

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -40,7 +40,6 @@ export default {
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	plugins: [
 		{ src: "@/plugins/v-scroll-lock.client.js", mode: "client" },
-		{ src: "@/plugins/vue-contenteditable.js" }
 	],
 
 	// Auto import components: https://go.nuxtjs.dev/config-components
--- client/plugins/vue-contenteditable.js
+++
@@ -1,4 +0,0 @@
-import Vue from "vue"
-import contenteditable from "vue-contenteditable"
-
-Vue.use(contenteditable)
