3f0d91c906ee37baf21cbd81798ffe91b98b5db1
Ayame Tatematsu <ayame.space@gmail.com>
2022-02-18 07:30:35 +0900

    generateのルーティングを生成できるように

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -111,6 +111,16 @@ export default {
 		}]
 	},
 
+
+	generate: {
+		async routes () {
+			const { $content } = require('@nuxt/content')
+			const files = await $content().only(['path']).fetch()
+
+			return files.map(file => file.path === '/index' ? '/' : file.path)
+		}
+	},
+
 	// Build Configuration: https://go.nuxtjs.dev/config-build
 	build: {},
 }
