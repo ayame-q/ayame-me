9d42e94dbf508450876febcb1897c897bd72d093
Ayame Tatematsu <ayame.space@gmail.com>
2022-03-03 20:51:02 +0900

    スキルの配置を移動、星を変更

--- client/components/SkillsList.vue
+++ client/components/SkillsList.vue
@@ -9,34 +9,34 @@
 					<img src="@/assets/img/coaster-illustrator.svg" alt="Illustrator">
 				</nuxt-link>
 			</li>
-			<li style="top: calc(2.5vw + 34.3vw); left: 28vw; width: 18vw;">
-				<nuxt-link to="/skills/vue-nuxt-js">
-					<img src="@/assets/img/coaster-vue.svg" alt="Vue/Nuxt.js">
-				</nuxt-link>
-			</li>
-			<li style="top: calc(2.5vw + 27.2vw); left: 48vw; width: 17vw;">
+			<li style="top: calc(2.5vw + 33.2vw); left: 28vw; width: 18vw;">
 				<nuxt-link to="/skills/html-css">
 					<img src="@/assets/img/coaster-htmlcss.svg" alt="HTML5/CSS3">
 				</nuxt-link>
 			</li>
+			<li style="top: calc(2.5vw + 27.5vw); left: 48vw; width: 17vw;">
+				<nuxt-link to="/skills/adobe-xd">
+					<img src="@/assets/img/coaster-xd.svg" alt="XD">
+				</nuxt-link>
+			</li>
 			<li style="top: calc(2.5vw + 24vw); left: 67vw; width: 14.5vw;">
-				<nuxt-link to="/skills/django">
-					<img src="@/assets/img/coaster-django.svg" alt="Django">
+				<nuxt-link to="/skills/vue-nuxt-js">
+					<img src="@/assets/img/coaster-vue.svg" alt="Django">
 				</nuxt-link>
 			</li>
-			<li style="top: calc(2.5vw + 21vw); left: 6vw; width: 14.5vw;">
-				<nuxt-link to="/skills/javascript">
-					<img src="@/assets/img/coaster-javascript.svg" alt="JavaScript">
-				</nuxt-link>
-			</li>
-			<li style="top: calc(2.5vw + 12.2vw); left: 22vw; width: 13.5vw;">
+			<li style="top: calc(2.5vw + 20.8vw); left: 6vw; width: 14.5vw;">
 				<nuxt-link to="/skills/photoshop">
 					<img src="@/assets/img/coaster-photoshop.svg" alt="Photoshop">
 				</nuxt-link>
 			</li>
+			<li style="top: calc(2.5vw + 13vw); left: 21vw; width: 13.5vw;">
+				<nuxt-link to="/skills/javascript">
+					<img src="@/assets/img/coaster-javascript.svg" alt="JavaScript">
+				</nuxt-link>
+			</li>
 			<li style="top: calc(2.5vw + 6.4vw); left: 35vw; width: 12vw;">
-				<nuxt-link to="/skills/adobe-xd">
-					<img src="@/assets/img/coaster-xd.svg" alt="XD">
+				<nuxt-link to="/skills/django">
+					<img src="@/assets/img/coaster-django.svg" alt="Django">
 				</nuxt-link>
 			</li>
 			<li style="top: calc(2.5vw - 0.7vw); left: 47vw; width: 11vw;">
